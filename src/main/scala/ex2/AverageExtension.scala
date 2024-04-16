9ae3e80864e136c08bf38a13d8a271cc91e67c06
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-16 16:30:20 +0200

    chore: refactor

--- src/main/scala/ex2/AverageExtension.scala
+++ src/main/scala/ex2/AverageExtension.scala
@@ -3,14 +3,8 @@ package ex2
 import scala.math.Numeric.Implicits._
 
 object ListExtensions:
-    implicit class ListOptionExtensions[T: Numeric](list: List[Option[T]]) {
-    def average: Option[Double] = {
-        val validScores = list.flatten
-        if (validScores.isEmpty) None
-        else Some(validScores.sum.toDouble / validScores.length)
-    }
-}
-
-
-
-  
+    implicit class ListOptionExtensions[T: Numeric](list: List[Option[T]]):
+        def average: Option[Double] =
+            val validScores = list.flatten
+            if (validScores.isEmpty) None
+            else Some(validScores.sum.toDouble / validScores.length)
